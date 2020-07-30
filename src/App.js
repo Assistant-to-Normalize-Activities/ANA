c4889c0990e415119769be3b52e099e12311fb9e
Esaú Morais <esau-morais@.github.com>
2020-07-30 19:18:57 -0300

    fix: remove mobile functions from desktop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { isMobile } from "react-device-detect";
 
 import Mobile from "./pages/Mobile/mobile";
+import Desktop from "./pages/Desktop/desktop";
 import Start from "./pages/Mobile/start";
 import QrCode from "./pages/Mobile/qrcode";
 import Protocols from "./pages/Mobile/protocols";
@@ -22,17 +23,7 @@ function App() {
 			</Router>
 		);
 	} else {
-		return (
-			<Router>
-				<Switch>
-					<Route exact path="/" component={Mobile} />
-					<Route path="/app" component={Start} />
-					<Route path="/qrcode" component={QrCode} />
-					<Route path="/protocolos" component={Protocols} />
-					<Route path="/chat" component={ChatBot} />
-				</Switch>
-			</Router>
-		);
+		return <Desktop />
 	}
 }
 
